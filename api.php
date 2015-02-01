35a47a1e827b221ffd1f343614b60d5071c500a4
Kiselev Dmitry <kiselev.d@i20.biz>
2015-02-01 13:55:33 +0600

    Remove files

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- */
-
-define('DRUPAL_ROOT', getcwd());
-require_once DRUPAL_ROOT . '/sites/all/modules/contrib/endpoint/includes/router.inc';
-// Include your endpoint routes here.
-// Example: require_once DRUPAL_ROOT . '/sites/all/modules/custom/my_module/my_module.endpoint.inc';
