c9b80bf8ebe0f33ada2b320088b39074361f978a
Andrew Mikhailov <andrew.mikhailov.18.01@gmail.com>
2015-02-05 23:50:36 +0600

    Add nodejs callbacks and Implements nodejs hooks.

---
+++ sites/all/modules/contrib/microservices/modules/ui/microservices_ui.js
@@ -0,0 +1,11 @@
+(function ($) {
+
+  Drupal.Nodejs.callbacks.microservices_ui_dashboard_js_callback = {
+    callback: function (message) {
+      Drupal.nodejs_ajax.runCommands(message);
+      $("#micro-db tr:even").removeClass('odd').addClass('even');
+      $("#micro-db tr:odd").removeClass('even').addClass('odd');
+    }
+  };
+
+}(jQuery));
